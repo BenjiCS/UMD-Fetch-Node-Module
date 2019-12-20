7bbc6119614d61efdc38a958dc5882c1c8b5911d
Benjamin Hansen <benjamin.hansen44@gmail.com>
2019-12-20 11:22:29 +0100

    remove test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-let nodeFetch = require("./umd");
-
-nodeFetch.init({
-  address: "https://reqres.in/api/users?page=2",
-  key: "1234"
-});
-
-nodeFetch.del("1").then(result => console.log(result));
